0349e70529970ee89a582734e5dd1ac4b41d4a1c
Derek McGowan <derek@mcgstyle.net>
2017-05-25 00:02:09 -0700

    Add support for shell dispatching and pty

--- dispatcher.go
+++ dispatcher.go
@@ -12,11 +12,25 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-type ExecHandler func([]string, *ssh.ServerConn, io.ReadWriteCloser) error
+type ConnectionSettings struct {
+	User        string
+	Permissions *ssh.Permissions
+	Env         map[string]string
+}
+
+type TerminalSettings struct {
+	Term          string
+	Width, Height uint32
+	Modes         map[Opcode]uint32
+}
+type ExecHandler func([]string, io.ReadWriteCloser, ConnectionSettings) error
+
+type ShellHandler func(ssh.Channel, ConnectionSettings, <-chan TerminalSettings)
 
 type Dispatcher struct {
 	c *ssh.ServerConfig
 
+	shell    ShellHandler
 	commands map[string]ExecHandler
 }
 
@@ -56,7 +70,14 @@ func (d *Dispatcher) handleConn(conn net.Conn) {
 	}
 	defer sc.Close()
 
-	go ssh.DiscardRequests(rc)
+	go func(in <-chan *ssh.Request) {
+		for req := range in {
+			logrus.Debugf("Discarding: %s", req.Type)
+			if req.WantReply {
+				req.Reply(false, nil)
+			}
+		}
+	}(rc)
 
 	for nc := range ncc {
 		if nc.ChannelType() != "session" {
@@ -73,50 +94,54 @@ func (d *Dispatcher) handleConn(conn net.Conn) {
 			break
 		}
 
-		hchan := make(chan execHandler)
-
-		go d.handleChannelRequests(rc, hchan)
-
-		go func() {
-			defer c.Close()
-
-			h := <-hchan
-
-			var responseStatus uint32
-			err = h(sc, closeChannelWriter{c})
-			if err != nil {
-				logrus.Debugf("Command failed: %+v", err)
-
-				fmt.Fprintf(c.Stderr(), "Command failed: %v\n", err)
-
-				// TODO: Check if has status function
-				responseStatus = 1
-			}
-
-			payload := make([]byte, 4)
-			binary.BigEndian.PutUint32(payload, responseStatus)
-			if _, err := c.SendRequest("exit-status", false, payload); err != nil {
-				logrus.Errorf("Send exit status error: %v", err)
-			}
-		}()
+		go d.handleChannelRequests(sc, c, rc)
 	}
 }
 
-type execHandler func(*ssh.ServerConn, io.ReadWriteCloser) error
+func (d *Dispatcher) handleChannelRequests(sc *ssh.ServerConn, c ssh.Channel, rc <-chan *ssh.Request) {
+	env := map[string]string{}
 
-func (d *Dispatcher) handleChannelRequests(rc <-chan *ssh.Request, hchan chan<- execHandler) {
+	tchan := make(chan TerminalSettings, 5)
 	for r := range rc {
 		var err error
 		var h func(*ssh.ServerConn, io.ReadWriteCloser) error
+		var envc map[string]string
 
 		switch r.Type {
 		case "env":
-			// TODO: Print envs
+			b := r.Payload
+			key, b := strVal(b)
+			value, b := strVal(b)
+			env[key] = value
+		case "window-change":
+			b := r.Payload
+			tw, b := intVal(b)
+			th, b := intVal(b)
+			_, b = intVal(b) // Terminal width, pixels
+			_, b = intVal(b) // Terminal height, pixels
+			tchan <- TerminalSettings{
+				Width:  tw,
+				Height: th,
+			}
 		case "shell":
-			err = errors.New("shell not supported")
+			envc = env
+		case "pty-req":
+			b := r.Payload
+			term, b := strVal(b)
+			tw, b := intVal(b)
+			th, b := intVal(b)
+			_, b = intVal(b) // Terminal width, pixels
+			_, b = intVal(b) // Terminal height, pixels
+			modes, b := modes(b)
+			tchan <- TerminalSettings{
+				Term:   term,
+				Width:  tw,
+				Height: th,
+				Modes:  modes,
+			}
 		case "exec":
-			l := binary.BigEndian.Uint32(r.Payload)
-			args, err := shlex.Split(string(r.Payload[4 : l+4]))
+			s, _ := strVal(r.Payload)
+			args, err := shlex.Split(s)
 			if err != nil {
 				err = errors.Wrap(err, "unable to parse exec command")
 			} else if len(args) == 0 {
@@ -125,13 +150,19 @@ func (d *Dispatcher) handleChannelRequests(rc <-chan *ssh.Request, hchan chan<- 
 				var f ExecHandler
 				f, err = d.lookupCommand(args[0])
 				if err == nil {
+					envc = env
+					env = map[string]string{}
 					h = func(sc *ssh.ServerConn, rw io.ReadWriteCloser) error {
-						return f(args, sc, rw)
+						return f(args, rw, ConnectionSettings{
+							User:        sc.User(),
+							Permissions: sc.Permissions,
+							Env:         envc,
+						})
 					}
 				}
 			}
 		default:
-			err = errors.Errorf("unknown type: %s", r.Type)
+			err = errors.Errorf("unhandled type: %s", r.Type)
 		}
 
 		if r.WantReply {
@@ -154,10 +185,40 @@ func (d *Dispatcher) handleChannelRequests(rc <-chan *ssh.Request, hchan chan<- 
 			continue
 		}
 		if h != nil {
-			hchan <- h
+			go func() {
+				defer c.Close()
+
+				var responseStatus uint32
+				err = h(sc, closeChannelWriter{c})
+				if err != nil {
+					logrus.Debugf("Command failed: %+v", err)
+
+					fmt.Fprintf(c.Stderr(), "Command failed: %v\n", err)
+
+					// TODO: Check if has status function
+					responseStatus = 1
+				}
+
+				payload := make([]byte, 4)
+				binary.BigEndian.PutUint32(payload, responseStatus)
+				if _, err := c.SendRequest("exit-status", false, payload); err != nil {
+					logrus.Errorf("Send exit status error: %v", err)
+				}
+			}()
+		} else if envc != nil {
+			go d.shell(c, ConnectionSettings{
+				User:        sc.User(),
+				Permissions: sc.Permissions,
+				Env:         envc,
+			}, tchan)
+
 		}
 
 	}
+}
+
+func (d *Dispatcher) ShellHandler(h ShellHandler) {
+	d.shell = h
 }
 
 func (d *Dispatcher) AddCommand(name string, h ExecHandler) {
@@ -179,4 +240,101 @@ type closeChannelWriter struct {
 
 func (c closeChannelWriter) Close() error {
 	return c.CloseWrite()
+}
+
+func strVal(b []byte) (string, []byte) {
+	if len(b) < 4 {
+		return "", b
+	}
+	l := binary.BigEndian.Uint32(b)
+	if len(b) < int(l)+4 {
+		return "", b
+	}
+	return string(b[4 : l+4]), b[l+4:]
+}
+
+func intVal(b []byte) (uint32, []byte) {
+	if len(b) < 4 {
+		return 0, b
+	}
+	return binary.BigEndian.Uint32(b), b[4:]
+}
+
+type Opcode byte
+
+const (
+	TTYOPEND    Opcode = iota // 0     TTY_OP_END  Indicates end of options.
+	VINTR                     // 1     VINTR       Interrupt character; 255 if none.
+	VQUIT                     // 2     VQUIT       The quit character (sends SIGQUIT signal on POSIX systems).
+	VERASE                    // 3     VERASE      Erase the character to left of the cursor.
+	VKILL                     // 4     VKILL       Kill the current input line.
+	VEOF                      // 5     VEOF        End-of-file character (sends EOF from the terminal).
+	VEOL                      // 6     VEOL        End-of-line character in addition to carriage return and/or linefeed.
+	VEOL2                     // 7     VEOL2       Additional end-of-line character.
+	VSTART                    // 8     VSTART      Continues paused output (normally control-Q).
+	VSTOP                     // 9     VSTOP       Pauses output (normally control-S).
+	VSUSP                     // 10    VSUSP       Suspends the current program.
+	VDSUSP                    // 11    VDSUSP      Another suspend character.
+	VREPRINT                  // 12    VREPRINT    Reprints the current input line.
+	VWERASE                   // 13    VWERASE     Erases a word left of cursor.
+	VLNEXT                    // 14    VLNEXT      Enter the next character typed literally, even if it is a special character
+	VFLUSH                    // 15    VFLUSH      Character to flush output.
+	VSWTCH                    // 16    VSWTCH      Switch to a different shell layer.
+	VSTATUS                   // 17    VSTATUS     Prints system status line (load, command, pid, etc).
+	VDISCARD                  // 18    VDISCARD    Toggles the flushing of terminal output.
+	IGNPAR      = 30          // 30    IGNPAR      The ignore parity flag.
+	PARMRK                    // 31    PARMRK      Mark parity and framing errors.
+	INPCK                     // 32    INPCK       Enable checking of parity errors.
+	ISTRIP                    // 33    ISTRIP      Strip 8th bit off characters.
+	INLCR                     // 34    INLCR       Map NL into CR on input.
+	IGNCR                     // 35    IGNCR       Ignore CR on input.
+	ICRNL                     // 36    ICRNL       Map CR to NL on input.
+	IUCLC                     // 37    IUCLC       Translate uppercase characters to lowercase.
+	IXON                      // 38    IXON        Enable output flow control.
+	IXANY                     // 39    IXANY       Any char will restart after stop.
+	IXOFF                     // 40    IXOFF       Enable input flow control.
+	IMAXBEL                   // 41    IMAXBEL     Ring bell on input queue full.
+	ISIG        = 40          // 50    ISIG        Enable signals INTR, QUIT, [D]SUSP.
+	ICANON                    // 51    ICANON      Canonicalize input lines.
+	XCASE                     // 52    XCASE       Enable input and output of uppercase characters by preceding their lowercase equivalents with "\".
+	ECHO                      // 53    ECHO        Enable echoing.
+	ECHOE                     // 54    ECHOE       Visually erase chars.
+	ECHOK                     // 55    ECHOK       Kill character discards current line.
+	ECHONL                    // 56    ECHONL      Echo NL even if ECHO is off.
+	NOFLSH                    // 57    NOFLSH      Don't flush after interrupt.
+	TOSTOP                    // 58    TOSTOP      Stop background jobs from output.
+	IEXTEN                    // 59    IEXTEN      Enable extensions.
+	ECHOCTL                   // 60    ECHOCTL     Echo control characters as ^(Char).
+	ECHOKE                    // 61    ECHOKE      Visual erase for line kill.
+	PENDIN                    // 62    PENDIN      Retype pending input.
+	OPOST       = 70          // 70    OPOST       Enable output processing.
+	OLCUC                     // 71    OLCUC       Convert lowercase to uppercase.
+	ONLCR                     // 72    ONLCR       Map NL to CR-NL.
+	OCRNL                     // 73    OCRNL       Translate carriage return to newline (output).
+	ONOCR                     // 74    ONOCR       Translate newline to carriage return-newline (output).
+	ONLRET                    // 75    ONLRET      Newline performs a carriage return  (output).
+	CS7         = 90          // 90    CS7         7 bit mode.
+	CS8                       // 91    CS8         8 bit mode.
+	PARENB                    // 92    PARENB      Parity enable.
+	PARODD                    // 93    PARODD      Odd parity, else even.
+	TTYOPISPEED = 128         // 128 TTY_OP_ISPEED  Specifies the input baud rate in bits per second.
+	TTYOPOSPEED               // 129 TTY_OP_OSPEED  Specifies the output baud rate in bits per second.
+)
+
+func modes(b []byte) (map[Opcode]uint32, []byte) {
+	if len(b) < 4 {
+		return nil, b
+	}
+	l := binary.BigEndian.Uint32(b)
+	if len(b) < int(l)+4 {
+		return nil, b
+	}
+	s := b[4 : l+4]
+	b = b[l+4:]
+
+	m := map[Opcode]uint32{}
+	for len(s) > 5 && s[0] > 0x00 && s[0] < 160 {
+		m[Opcode(s[0])], s = intVal(s[1:])
+	}
+	return m, b
 }
